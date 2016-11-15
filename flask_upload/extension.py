3777d5fe7df0b823725b73b9f890b599894fc117
by46 <ycs_ctbu_2010@126.com>
2016-11-15 17:25:06 +0800

    upload file to qiniu

--- flask_upload/extension.py
+++ flask_upload/extension.py
@@ -1,6 +1,17 @@
+import httplib
+
+import requests
+from qiniu import Auth
+from qiniu import put_data
+
 from flask_upload.views import upload_blueprint
 
-URL_PREFIX = 'x'
+URL_PREFIX = 'url_prefix'
+ACCESS_KEY = 'qiniu_access_key'
+SECRET_KEY = 'qiniu_secret_key'
+BUCKET = 'bucket'
+HOST = 'qiniu_cdn'
+
 missing = object()
 
 
@@ -8,13 +19,23 @@ class Upload(object):
     def __init__(self, app=None, **kwargs):
         self._options = {}
         self._app = None
+        self._access_key = None
+        self._secret_key = None
+        self._bucket = None
+        self._host = None
         if app is not None:
             self.init_ap(app, **kwargs)
 
     def init_app(self, app, **kwargs):
         self._app = app
         self._options.update(kwargs)
-        upload_blueprint.url_prefix = self.get_parameter('url_prefix')
+        self._access_key = self.get_parameter(ACCESS_KEY)
+        self._secret_key = self.get_parameter(SECRET_KEY)
+        self._bucket = self.get_parameter(BUCKET)
+        self._host = self.get_parameter(HOST)
+
+        self._app.upload = self
+        upload_blueprint.url_prefix = self.get_parameter(URL_PREFIX)
         app.register_blueprint(upload_blueprint)
 
     def get_parameter(self, name, default=None):
@@ -23,3 +44,21 @@ class Upload(object):
             parameter_name = 'UPLOAD_{0}'.format(name.upper())
             option = self._app.config.get(parameter_name, default)
         return option
+
+    def get_auth(self):
+        pass
+
+    def __make_url(self, key):
+        return "{0}/{1}".format(self._host, key)
+
+    def upload(self, key, content, mime_type):
+        url = self.__make_url(key)
+        response = requests.head(url)
+        if response.status_code == httplib.OK:
+            return response.headers.get('etag').strip("\"")
+        q = Auth(self._access_key, self._secret_key)
+        token = q.upload_token(self._bucket, key, 3600)
+        ret, response = put_data(token, key, content, mime_type=mime_type)
+        if response.status_code == httplib.OK:
+            return url, ret.get('hash')
+        return None, None
--- flask_upload/views/__init__.py
+++ flask_upload/views/__init__.py
@@ -1,15 +1,12 @@
 import hashlib
-import httplib
 import mimetypes
 import os.path
 
-import requests
 from flask import Blueprint
+from flask import current_app as app
 from flask import render_template
 from flask_wtf import Form
 from flask_wtf.file import FileField
-from qiniu import Auth
-from qiniu import put_data
 from werkzeug.utils import secure_filename
 
 
@@ -25,42 +22,16 @@ upload_blueprint = Blueprint('_uploader', __name__, template_folder=os.path.join
 def upload_file():
     form = ImageForm()
     if form.validate_on_submit():
+        uploader = app.upload
+        storage = form.picture.data
         filename = secure_filename(form.picture.data.filename)
-        with open('uploads/' + filename, 'wb') as writer:
-            # while True:
-            data = form.picture.data.read(12)
-            writer.write(data)
-
-        # form.picture.data.save('uploads/' + filename)
-        picture_url = None
-        file_hash = 'file hash'
+        __, ext = os.path.splitext(filename)
+        mime, __ = mimetypes.guess_type(filename)
+        mime = mime or 'html/plain'
+        content = storage.read()
+        key = '{0}{1}'.format(hashlib.sha256(content).hexdigest(), ext)
+        picture_url, file_hash = uploader.upload(key, content, mime)
         return render_template('upload_result.html', picture_url=picture_url, file_hash=file_hash)
 
     else:
         return render_template('upload.html', form=form)
-
-
-def handle_uploaded_file2(key, data, mime_type):
-    url = self.make_url(key)
-    response = requests.head(url)
-    if response.status_code == httplib.OK:
-        return response.headers.get('etag').strip("\"")
-    q = Auth(self._access_key, self._secret_key)
-    token = q.upload_token(self._bucket_name, key, 3600)
-    ret, response = put_data(token, key, data, mime_type=mime_type)
-    if response.status_code == httplib.OK:
-        return ret.get('hash')
-    return None
-
-
-def handle_uploaded_file(entity):
-    __, ext = os.path.splitext(entity.name)
-    mime, __ = mimetypes.guess_type(entity.name)
-    mime = mime or 'html/plain'
-    content = entity.read()
-    key = '{0}{1}'.format(hashlib.sha256(content).hexdigest(), ext)
-    file_hash = client.upload(key, content, mime)
-    if file_hash:
-        return client.make_url(key), file_hash
-    else:
-        return None, None
