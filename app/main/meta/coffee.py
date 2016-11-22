ab2761017fccd3d92874db2d0df75653dfc8fa63
by46 <ycs_ctbu_2010@126.com>
2016-11-22 17:41:16 +0800

    add swagger

--- app/main/meta/coffee.py
+++ app/main/meta/coffee.py
@@ -4,9 +4,15 @@ from flask_restful_swagger import swagger
 
 @swagger.model
 class CoffeeModel(object):
+    """
+
+    """
     resource_fields = {
         "name": fields.String,
         "on_sale_date": fields.DateTime,
         "imported": fields.Boolean,
         "imported2": fields.Boolean
+    }
+    swagger_metadata = {
+        'name': {'required': True}
     }
