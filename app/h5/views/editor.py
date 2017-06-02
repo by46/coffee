e256bd253229450164bdb5c120ca3771f54d0999
by46 <ycs_ctbu_2010@126.com>
2017-06-02 08:27:39 +0800

    feat: fix bug

--- app/h5/views/editor.py
+++ app/h5/views/editor.py
@@ -3,6 +3,8 @@ from flask import render_template
 from app.h5 import h5
 
 
-@h5.route('/preview', methods=['GET'])
-def preview():
+@h5.route('/preview2/<stock_or_code>', methods=['GET'])
+@h5.route('/preview/<stock_or_code>', methods=['GET'])
+def preview(stock_or_code):
+    print(stock_or_code)
     return render_template('h5/preview.html')
